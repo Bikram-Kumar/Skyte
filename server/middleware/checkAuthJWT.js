d170cbc7c50d4ab4c62a300fe5d2629834097294
Bikram Kumar <coolbikram0@gmail.com>
2025-03-25 16:20:14 +0530

    auth checkup

--- server/middleware/checkAuthJWT.js
+++ server/middleware/checkAuthJWT.js
@@ -1,7 +1,15 @@
 import "dotenv/config";
 import { auth } from "express-oauth2-jwt-bearer";
 
-export default auth({
-    audience: 'https://skyte-api',
-    issuerBaseURL: process.env.ISSUER_BASE_URL
-});
+export default function checkAuthJWT(req, res, next) {
+    try {
+        auth({
+            audience: 'https://skyte-api',
+            issuerBaseURL: process.env.ISSUER_BASE_URL
+        });
+    } catch(e) {
+        console.log(e);
+        console.log(req);
+    }
+    next();
+};
